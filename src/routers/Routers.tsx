a9ee9eddfa14584c1fd2390b1f835d2eedd8c589
Asranov Baxtinur <asranov0003@gmail.com>
2023-09-29 10:04:18 +0500

    add auth routes

--- src/routers/Routers.tsx
+++ src/routers/Routers.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from '../layout/Layout'
+import Login from '../pages/auth/Login/Login'
+import Register from '../pages/auth/Register/Register'
 import Blog from '../pages/Blog/Blog'
 import Home from '../pages/Home/Home'
 import NotFoundPage from '../pages/NotFoundPage/NotFoundPage'
@@ -16,6 +18,8 @@ function Routers() {
           <Route path='/shop' element={<Shop />} />
           <Route path='/blog' element={<Blog />} />
           <Route path='/ourstory' element={<OurStory />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
           <Route path='*' element={<NotFoundPage />} />
         </Route>
       </Routes>
